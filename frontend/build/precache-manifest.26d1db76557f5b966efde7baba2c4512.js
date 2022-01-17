3479a819619cc8b0bd879bc78358798baa4691d2
sofia <sofia.castro@usach.cl>
2022-01-16 22:35:09 -0300

    Se cambia el contenido de jenkinsfile.

---
+++ frontend/build/precache-manifest.26d1db76557f5b966efde7baba2c4512.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb5c3f53a0bd89204d60",
+    "url": "/static/css/main.dd31841a.chunk.css"
+  },
+  {
+    "revision": "bb5c3f53a0bd89204d60",
+    "url": "/static/js/main.bb5c3f53.chunk.js"
+  },
+  {
+    "revision": "6de07c8508ff563140b6",
+    "url": "/static/js/1.6de07c85.chunk.js"
+  },
+  {
+    "revision": "78ff34968ac6f9e1bd41",
+    "url": "/static/css/2.179240b5.chunk.css"
+  },
+  {
+    "revision": "78ff34968ac6f9e1bd41",
+    "url": "/static/js/2.78ff3496.chunk.js"
+  },
+  {
+    "revision": "97a47464389a77d60471",
+    "url": "/static/js/runtime~main.97a47464.js"
+  },
+  {
+    "revision": "7517e8af3c2b9fcdb5d16c23b70f1ca1",
+    "url": "/index.html"
+  }
+];
